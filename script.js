96a03b5e25aa40c28820e3d9011616c5cd62eab7
Alam Talash <alamtalash@Alams-MBP.hitronhub.home>
2021-02-11 22:25:01 -0800

    Send fuction is created.

--- script.js
+++ script.js
@@ -10,18 +10,30 @@ const send=(e)=>{
   e.preventDefault(); 
   let fails = validate(); 
   if(fails.length === 0){
-    console.log("Type some thing. ")
+    document.querySelector('#form').submit(); 
+    console.log("Thank you for submitting your data!"); 
+  } else {
+    fails.forEach(obj => {
+      console.log("object", obj); 
+      let p = document.createElement('p');
+      let field = document.getElementById(obj.inputID); 
+      console.log("Child nodes are; ",  field)
+      let x = field.parentElement.append(p);
+      p.innerHTML = obj.errorMSG; 
+      p.classList.add('error');
+      // field.parentElement.setAttribute('data-er', obj.errorMSG);
+    })
   }
 }
  const validate = () =>{
   let fname = document.querySelector('#fname'); 
-  let failure = []; 
+  let failures = []; 
   if(fname.value ===''){
-    failure.push({
-      input: 'input',
-      msg: 'Please add your name.'
+    failures.push({
+      inputID: 'fname',
+      errorMSG: 'Please add your name.'
     })
   }
-  return failure; 
+  return failures; 
  }
 window.addEventListener('DOMContentLoaded', init); 
